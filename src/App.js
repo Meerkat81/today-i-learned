2ff18713d94a0378d459a406fe7a89b0b9ee13d7
Casey Meurer <Casey.Meurer19@hotmail.com>
2023-09-16 17:22:15 +0200

    make db call

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import supabase from "./supabase";
 import "./style.css";
 
@@ -35,10 +35,16 @@ const initialFacts = [
     createdIn: 2015,
   },
 ];
-
 function App() {
   const [showForm, setShowForm] = useState(false);
-  const [facts, setFacts] = useState(initialFacts);
+  const [facts, setFacts] = useState([]);
+  useEffect(function () {
+    async function getFacts() {
+      const { data: facts, error } = await supabase.from("facts").select("*");
+      setFacts(facts);
+    }
+    getFacts();
+  }, []);
   return (
     <>
       <Header showForm={showForm} setShowForm={setShowForm} />
@@ -184,7 +190,6 @@ function CategoryFilter() {
 
 function FactList({ facts }) {
   // TEMPORARY
-
   return (
     <section>
       <ul className="facts-list">
