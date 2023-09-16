5bfcb4be10813e57077ff37a2be54ec7f02d9b8c
Casey Meurer <Casey.Meurer19@hotmail.com>
2023-09-16 17:21:45 +0200

    use react en name

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-
 const supabaseUrl = "https://fcftmlbyprvezwswyxti.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
