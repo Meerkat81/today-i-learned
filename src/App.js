d147b193f887a054293a06bbbdf3dc1f7e9a2c14
Casey Meurer <Casey.Meurer19@hotmail.com>
2023-09-16 11:18:18 +0200

    move over styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import "./style.css";
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" alt="Today I Learned Logo" />
+        <h1>Today I Learned</h1>
+      </div>
+      <button className="btn btn-large btn-open">Share a Fact</button>
+    </header>
+  );
 }
 
 export default App;
