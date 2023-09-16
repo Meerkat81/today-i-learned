7d73e9f282b5a61957a80f5c3935768228a82b07
Casey Meurer <Casey.Meurer19@hotmail.com>
2023-09-16 18:01:01 +0200

    return message if no facts exist for cat

--- src/App.js
+++ src/App.js
@@ -216,7 +216,13 @@ function CategoryFilter({ setCurrectCategory }) {
 }
 
 function FactList({ facts }) {
-  // TEMPORARY
+  if (facts.length === 0) {
+    return (
+      <p className="message">
+        No facts for this category yet! Create the first one ✌️
+      </p>
+    );
+  }
   return (
     <section>
       <ul className="facts-list">
