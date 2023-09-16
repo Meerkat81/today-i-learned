44fab91d4828f707636f827b0615a94bd3b88586
Casey Meurer <Casey.Meurer19@hotmail.com>
2023-09-16 11:35:59 +0200

    configure struture in react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import "./style.css";
 function App() {
   return (
-    <header className="header">
-      <div className="logo">
-        <img src="logo.png" alt="Today I Learned Logo" />
-        <h1>Today I Learned</h1>
-      </div>
-      <button className="btn btn-large btn-open">Share a Fact</button>
-    </header>
+    <>
+      <header className="header">
+        <div className="logo">
+          <img src="logo.png" alt="Today I Learned Logo" />
+          <h1>Today I Learned</h1>
+        </div>
+        <button className="btn btn-large btn-open">Share a Fact</button>
+      </header>
+      <NewFactForm />
+      <main class="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
   );
+}
+
+function NewFactForm() {
+  return <form className="fact-form">Fact Form</form>;
+}
+
+function CategoryFilter() {
+  return <aside>Category Filter</aside>;
+}
+
+function FactList() {
+  return <section>Facts list</section>;
 }
 
 export default App;
