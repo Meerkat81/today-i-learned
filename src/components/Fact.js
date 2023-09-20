17894302e3172cfd4646b64ffe33b4d751ee9b9b
Meerkat81 <57145227+Meerkat81@users.noreply.github.com>
2023-09-20 13:02:54 +0200

    break out into components

---
+++ src/components/Fact.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import supabase from "../supabase";
+import { CATEGORIES } from "../App";
+
+export function Fact({ fact, setFacts }) {
+  const [isUpdating, setIsUpdating] = useState(false);
+  const isDisputed =
+    fact.votesInteresting + fact.votesMindBlowing < fact.voteFalse;
+  async function handleVote(columnName) {
+    setIsUpdating(true);
+    const { data: updatedFact, error } = await supabase
+      .from("facts")
+      .update({ [columnName]: fact[columnName] + 1 })
+      .eq("id", fact.id)
+      .select();
+    setIsUpdating(false);
+    if (!error)
+      setFacts((facts) =>
+        facts.map((f) => (f.id === fact.id ? updatedFact[0] : f))
+      );
+  }
+
+  const categoryColor = (catArray, category) =>
+    catArray.find((cat) => cat.name === category.category).color;
+  return (
+    <li className="fact">
+      {isDisputed && <span className="disputed">[⛔️DISPUTED]</span>}
+      <p>
+        {fact.text}
+        <a
+          className="source"
+          href={fact.source}
+          target="_blank"
+          rel="noreferrer"
+        >
+          (Source)
+        </a>
+      </p>
+      <span
+        className="tag"
+        style={{
+          backgroundColor: `${categoryColor(CATEGORIES, fact)}`,
+        }}
+      >
+        {fact.category}
+      </span>
+      <div className="vote-buttons">
+        <button
+          onClick={() => handleVote("votesInteresting")}
+          disabled={isUpdating}
+        >
+          👍 {fact.votesInteresting}
+        </button>
+        <button
+          onClick={() => handleVote("votesMindBlowing")}
+          disabled={isUpdating}
+        >
+          🤯 {fact.votesMindBlowing}
+        </button>
+        <button onClick={() => handleVote("voteFalse")} disabled={isUpdating}>
+          ⛔️ {fact.voteFalse}
+        </button>
+      </div>
+    </li>
+  );
+}
